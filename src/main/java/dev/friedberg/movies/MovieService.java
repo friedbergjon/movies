05732804ceb41311b2eaf47e5297ffdbca35db34
Jonathan Friedberg <friedberg_jon@hotmail.com>
2024-02-09 23:45:48 -0600

    find 1 movie

--- src/main/java/dev/friedberg/movies/MovieService.java
+++ src/main/java/dev/friedberg/movies/MovieService.java
@@ -1,8 +1,10 @@
 package dev.friedberg.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,4 +15,8 @@ public class MovieService {
     public List<Movie> allMovies(){
         return movieRepository.findAll();
     }
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
+    }
 }
+
